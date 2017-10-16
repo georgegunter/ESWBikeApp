730922bcad35890ffac3e68493686fa5f33b2f86
David Bergvelt <bergvel1@illinois.edu>
2017-10-16 10:38:58 -0500

    Map screen (using osmdroid) and preride screen

--- app/src/main/java/com/example/cubikerouting/RideActivity.java
+++ app/src/main/java/com/example/cubikerouting/RideActivity.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
+import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
 public class RideActivity extends AppCompatActivity {
